85afc4daee24f77e33c9e4d224394e52a48104c4
Guevara, Alexei <alguevara@ebay.com>
2014-09-24 01:25:01 -0400

    nice

--- src/counters_clj/aggregator.clj
+++ src/counters_clj/aggregator.clj
@@ -1,10 +1,7 @@
 (ns counters-clj.aggregator
-  (:require [clojure.core.async :as async :refer [chan go <! >! >!!]]))
+  (:require [clojure.core.async :as async :refer [chan go <! >! >!! <!! alt!! alts!!]]))
 
-; message source
-
-; message offset
-
+;; TODO: graphite!
 
 ; coordinator
 
@@ -18,36 +15,65 @@
 ;; {:action :flush, {...}}
 
 
-(defn aggregate [a v f]
+(defn aggregator [af init-state ff]
   "
-  'a' function that aggregates the value of event into result
-  'f' function that writes result and event offset to db
+  'af' function that aggregates the value of event into result
+  'ff' function that writes result and event offset to db
+
+  returns a channel this aggregator will process messages from
   "
   (let [c (chan)]
     (go
-     (loop [state {}
+     (loop [state init-state
             {:keys [action value offset] :as event} {:action :boot}]
-       (case action
-         :boot (recur {:result v :offset 0} (<! c))
-         :process (recur {:result (a (:result state) value) :offset offset}
-                         (<! c))
-         :flush (do (f state)
-                  (recur state
-                         (<! c))))))
+       (when event
+         (case action
+           :boot (recur init-state (<! c))
+           :process (recur {:result (af (:result state) value) :offset offset}
+                           (<! c))
+           :flush (do (ff state)
+                      (recur state (<! c)))
+           (recur state (<! c))))))
+    c))
+
+(defn fetch [f init-offset period c]
+  "'period' in milliseconds
+  'f' function to sample
+  'c' channel to write sampled value to
+
+  TODO: stop sampling when output channel is closed
+  "
+  (go (loop [offset init-offset]
+        (when (>! c (f offset))
+          (<! (async/timeout period))
+          (recur (inc offset))))))
+
+(defn sample [f period c]
+  (go (loop []
+        (when (>! c (f))
+          (<! (async/timeout period))
+          (recur)))))
+
+(defn start-aggregator [sf
+                        ef ef-period
+                        af
+                        ff ff-period]
+  (let [{:keys [offset] :as init-state} (sf)
+        c (aggregator af init-state ff)]
+    (fetch ef offset ef-period c)
+    (sample (fn [] {:action :flush}) ff-period c)
     c))
 
 
-
-(def c (aggregate (fn [r v]
-                    (println (str "AGREGATE -> r:" r " v:" v))
-                    (+ r v))
-                  0
-                  (fn [{:keys [result offset] :as state}]
-                    (println (str "FLUSH: -> " "result:" result " offset: " offset)))))
+(defn lala [i] (start-aggregator (fn [] {:result 0 :offset 0})
+                         (fn [offset] {:action :process :value 3 :offset (inc offset)}) 10
+                         (fn [r v] (+ r v))
+                         (fn [{:keys [result offset] :as state}]
+                           (println (str "FLUSH: -> " "result:" result " offset: " offset))) 5000))
 
 
+(def cs (map #(lala %) (range 50)))
 
-(>!! c {:action :process :value 1001 :offset 4})
+(doall cs)
 
-(>!! c {:action :flush})
-
+(doseq [c cs] (async/close! c))
