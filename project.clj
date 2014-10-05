947266494d43257515e03abbdb324dbd733091d6
Alexei Guevara <alexguev@gmail.com>
2014-10-05 19:21:35 -0400

    progress merging aggregate and flush

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.kafka/kafka_2.10 "0.8.1.1" :exclusions [[com.sun.jmx/jmxri] [com.sun.jdmk/jmxtools] [javax.jms/jms]]]
                  [clj-kafka "0.2.6-0.8" :exclusions [[org.apache.kafka/kafka_2.10] [org.apache.zookeeper/zookeeper]]]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ragtime/ragtime.sql.files "0.3.6"]
                  [org.clojure/java.jdbc "0.3.5"]
                  [mysql/mysql-connector-java "5.1.32"]
--- src/agg/core.clj
+++ src/agg/core.clj
@@ -1,5 +1,5 @@
 (ns agg.core
-  (:require [clojure.core.async :as async :refer [chan go <! >! sliding-buffer close!]]))
+  (:require [clojure.core.async :as async :refer [chan go <! >! go-loop alts! timeout]]))
 
 (defn do-agg [chi f init cho n]
   "
@@ -21,6 +21,63 @@
             :flush (do (>! cho state)
                        (recur state (<! chi) 0))
             (recur state (<! chi) i))))))
+
+(defn stage-agg [in f init out]
+  "
+  'in' input channel, events will be read from it
+  'f' function that aggregates the value of events into a result
+  'init' initial state
+  'out' output channel, aggregate results will be writen to it
+  "
+  (go (loop [state init
+             {:keys [boot value offset] :as message} {:boot true}]
+        (when message
+          (if (true? boot)
+            (recur init (<! in))
+            (let [new-state {:result (f (:result state) value) :offset offset}]
+              (>! out new-state)
+              (recur new-state (<! in))))))))
+
+(defn when-fn [x f g]
+  (if (f x) (g x) x))
+
+(defn stage-flush [in out n msecs]
+  (go (loop [state {:i 0}]
+        (println (str "state = " state))
+        (let [t (timeout msecs)
+              [v ch] (alts! [in t])]
+          (if-not (nil? v)
+            (recur (-> state
+                       (assoc :message v)
+                       (update-in [:i] inc)
+                       (when-fn (fn [{:keys [i]}] (and (> i 0) (zero? (rem i n))))
+                                (fn [{:keys [i message] :as s}]
+                                  (async/put! out message)
+                                  (assoc s :flushed-at i)))))
+            (let [{:keys [i message]} state]
+              (when (> i (get state :flushed-at 0))
+                (>! out (:message state)))
+              (when (= ch t)
+                (recur (assoc state :flushed-at i)))))))))
+
+;;        (when
+;;         (>! out state))
+
+;; (def in1 (chan 100))
+;; (def out1 (chan 100))
+;; (stage-agg in1 + {:result 0} out1)
+
+;; (async/>!! in1 {:value 1110 :offset 12})
+
+;; (def out2 (chan 100))
+
+;; (stage-flush out1 out2 5 10000)
+
+;; (go (loop [v nil]
+;;       (println (str "flush!"))
+;;       (recur (<! out2))))
+
+
 
 (defn sample-iterate [f init-offset period ch]
   "'period' in milliseconds
