dd5ea2601059a222c17a9ae89069ce79b700f549
Alexei Guevara <alexguev@gmail.com>
2014-10-08 00:49:34 -0400

    ahora si

--- src/ad_view_counter/counter.clj
+++ src/ad_view_counter/counter.clj
@@ -10,10 +10,10 @@
          :user "agg"
          :password "agg"})
 
-(defn flush-state [partition-id counter-type {:keys [result offset] :as state}]
+(defn flush-state [partition-id counter-type {:keys [delta offset] :as state}]
   (let [now (c/to-date (t/now))]
     (jdbc/with-db-transaction [conn db :isolation :read-committed]
-        (doseq [[ad-id value] result] ;; one per transaction for now, support batching later
+        (doseq [[ad-id value] delta] ;; one per transaction for now, support batching later
           (jdbc/execute! conn [(str "INSERT INTO ad_counter(ad_id, type, value, modification_date, creation_date)"
                                     " VALUES (?,?,?,?,?)"
                                     " ON DUPLICATE KEY UPDATE value = ?, modification_date = ?")
@@ -24,9 +24,11 @@
 
 (def counter (atom 0))
 
-(defn process-event [r ad-id]
+(defn increment-counter [[result delta] ad-id]
   (swap! counter dec)
-  (update-in r [ad-id] #(if % (inc %) 1)))
+  (let [new-result (update-in result [ad-id] #(if % (inc %) 1))
+        new-delta (assoc delta ad-id (get new-result ad-id))]
+    [new-result new-delta]))
 
 
 (defn fetch-event [partition-id offset]
@@ -34,35 +36,35 @@
     (swap! counter inc)
     {:value (+ (* partition-id n) (rand-int n)) :offset (inc offset)}))
 
-;(defn agg [in f init out n msecs]
+;;(defn agg [in f init out n msecs]
 
 (defn start-counting [counter-type partition-id]
-  (let [in (chan 128)
-        out (chan 128)]
-    (agg in process-event {:result {} :offset 0} out 256 (* 30 1000))
+  (let [in (chan 16)
+        out (chan 16)]
+    (agg in increment-counter {} out 128 (* 5 1000))
     (sample-from-offset (partial fetch-event partition-id) 0 10 in)
     (subscribe out (partial flush-state partition-id counter-type))
     in)
   )
 
 
-(def c1 (start-counting "view" 0))
-(def c2 (start-counting "view" 1))
+(def c1 (start-counting "view" 1))
+(def c2 (start-counting "view" 2))
 (def c3 (start-counting "view" 3))
 (def c4 (start-counting "view" 4))
 (def c5 (start-counting "view" 5))
-;; (def c6 (start-counting "view" 6))
-;; (def c7 (start-counting "view" 7))
-;; (def c8 (start-counting "view" 8))
-;; (def c9 (start-counting "view" 9))
-;; (def c10 (start-counting "view" 10))
-;; (def c11 (start-counting "view" 11))
-;; (def c12 (start-counting "view" 12))
-;; (def c13 (start-counting "view" 13))
-;; (def c13 (start-counting "view" 14))
-;; (def c14 (start-counting "view" 15))
+(def c6 (start-counting "view" 6))
+(def c7 (start-counting "view" 7))
+(def c8 (start-counting "view" 8))
+(def c9 (start-counting "view" 9))
+(def c10 (start-counting "view" 10))
+(def c11 (start-counting "view" 11))
+(def c12 (start-counting "view" 12))
+(def c13 (start-counting "view" 13))
+(def c14 (start-counting "view" 14))
+(def c15 (start-counting "view" 15))
 
-(close! c5)
+(close! c13)
 
 @counter
 
--- src/agg/core.clj
+++ src/agg/core.clj
@@ -1,43 +1,33 @@
 (ns agg.core
   (:require [clojure.core.async :as async :refer [chan go <! >! go-loop alts! timeout]]))
 
-(defn stage-agg [in f init out]
+(defn agg [in f init out n msecs]
   "
   'in' input channel, events will be read from it
   'f' function that aggregates the value of events into a result
   'init' initial state
   'out' output channel, aggregate results will be writen to it
   "
-  (go (loop [state init
-             {:keys [boot value offset] :as message} {:boot true}]
-        (when message
-          (if (true? boot)
-            (recur init (<! in))
-            (let [new-state {:result (f (:result state) value) :offset offset}]
-              (>! out new-state)
-              (recur new-state (<! in))))))))
-
-
-(defn stage-flush [in out n msecs]
-  (go (loop [state {:i 0}]
+  (go (loop [{:keys [result delta i flushed-at] :as state} {:result init :i 0 :flushed-at 0}]
+        ;;(println state)
         (let [t (timeout msecs)
-              [message ch] (alts! [in t])]
-          (if-not (nil? message)
-            (let [{:keys [i] :as new-state} (-> state (assoc :message message) (update-in [:i] inc))]
-              (if (and (> i 0) (zero? (rem i n)))
-                (do (>! out message) (recur (assoc new-state :flushed-at i)))
+              [{:keys [value offset] :as message} ch] (alts! [in t])]
+          (if (nil? message)
+            (if (= ch t)
+              (if (> i flushed-at)
+                (do (>! out state) (recur (-> state (dissoc :delta) (assoc :flushed-at i))))
+                (recur state))
+              (when (> i flushed-at) (>! out state)))
+            (let [[new-result new-delta] (f [result delta] value)
+                  {new-i :i :as new-state} (-> state
+                                               (assoc :result new-result)
+                                               (assoc :delta new-delta)
+                                               (assoc :offset offset)
+                                               (update-in [:i] inc))]
+              (if (zero? (rem new-i n))
+                (do (>! out new-state) (recur (-> new-state (dissoc :delta) (assoc :flushed-at new-i))))
                 (recur new-state)))
-            (let [{:keys [i message]} state]
-              (if (= ch t)
-                (if (> i (get state :flushed-at 0))
-                  (do (>! out message) (recur (assoc state :flushed-at i)))
-                  (recur state))
-                (when (> i (get state :flushed-at 0)) (>! out message)))))))))
-
-(defn agg [in f init out n msecs]
-  (let [in-out (chan (async/sliding-buffer 1))]
-    (stage-agg in f init in-out)
-    (stage-flush in-out out n msecs)))
+            )))))
 
 (defn sample-from-offset [f init-offset period ch]
   "
