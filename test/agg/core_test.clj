66ecad825e6576345577fe966ab505c99720463d
Alexei Guevara <alexguev@gmail.com>
2014-10-16 00:43:07 -0400

    yeeehhh added some unit tests

--- test/agg/core_test.clj
+++ test/agg/core_test.clj
@@ -1,7 +1,54 @@
-(ns counters-clj.core-test
+(ns agg.core-test
   (:require [clojure.test :refer :all]
-            [counters-clj.core :refer :all]))
+            [agg.core :refer :all]
+            [clojure.core.async :refer [chan go <!! >!! alts!! timeout close! onto-chan]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest put-state-in-out-channel
+
+  (testing "after processing n messages"
+    (let [in (chan)
+          out (chan)
+          x 10]
+      (agg in (fn [[r d] v] [(+ r v) (+ r v)]) 0 out x 1000)
+      (onto-chan in (map #(hash-map :value % :offset %) (range x)) false)
+      (let [[val _] (alts!! [out (timeout 1000)])]
+        (is (= (apply + (range x))
+               (:delta val)))
+        (is (= (apply + (range x))
+               (:result val)))
+        (is (= (- x 1)
+               (:offset val))))
+      (close! in)))
+
+  (testing "after timed out waiting for a message"
+    (let [in (chan)
+          out (chan)
+          messages [{:value 3 :offset 0} {:value 0.14159 :offset 1}]]
+      (agg in (fn [[r d] v] [(+ r v) (+ r v)]) 0 out 9999 100)
+      (onto-chan in messages false)
+      (let [[val _] (alts!! [out (timeout 1000)])]
+        (is (= (apply + (map :value messages))
+               (:delta val)))
+        (is (= (apply + (map :value messages))
+               (:result val)))
+        (is (= (- (count messages) 1)
+               (:offset val))))
+      (close! in)))
+
+  (testing "when in channel is closed"
+    (let [in (chan)
+          out (chan)
+          messages [{:value 3 :offset 0} {:value 0.14159 :offset 1}]]
+      (agg in (fn [[r d] v] [(+ r v) (+ r v)]) 0 out 9999 100000)
+      (<!! (onto-chan in messages))
+      (close! in)
+      (let [[val _] (alts!! [out (timeout 1000)])]
+        (is (= (apply + (map :value messages))
+               (:delta val)))
+        (is (= (apply + (map :value messages))
+               (:result val)))
+        (is (= (- (count messages) 1)
+               (:offset val)))))))
+
+(run-tests)
